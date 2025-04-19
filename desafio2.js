7b08d254d7c3796e68c3802a64a189e98b3c1a78
Gustavo Brandão <gustavo1731@hotmail.com>
2025-04-19 20:36:50 -0300

    Update desafio2.js

---
+++ desafio2.js
@@ -0,0 +1,23 @@
+//Desafio 1
+function novafuncao(array1, array2) {
+    const arrayUniao = array1.concat(array2);
+    return arrayUniao;
+}
+
+const numeros1 = [1, 2, 3];
+const numeros2 = [4, 5, 6];
+
+const resultado = novafuncao(numeros1, numeros2);
+console.log(resultado);
+
+
+//Desafio 2
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const parteNumeros = numeros.slice(3,7);
+
+
+//Desafio 3
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi'];
+
+frutas.splice(2, 2, 'Kiwi', 'Pêssego');
+console.log(frutas);

e463f7cd14e54b7b32be180f50907ee1c3c7d5c5
Gustavo Brandão <gustavo1731@hotmail.com>
2025-04-19 19:32:02 -0300

    Create atualizar-lista.js

---
+++ atualizar-lista.js
@@ -0,0 +1,5 @@
+const listaEstudantes= ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+listaEstudantes.splice(1, 2, 'Rodrigo'); // primeiro parâmetros é o start, segundo a quantidade de itens deletado e o terceiro adiciona.
+
+console.log(listaEstudantes);

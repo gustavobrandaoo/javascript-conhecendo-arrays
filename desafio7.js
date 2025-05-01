1390d9cb4c98b9ccc52279f9843e44f536eeef80
Gustavo Brandão <gustavo1731@hotmail.com>
2025-05-01 19:17:36 -0300

    Update desafio7.js

---
+++ desafio7.js
@@ -0,0 +1,27 @@
+//Desafio 1: Crie um objeto chamado pessoa que represente informações sobre uma pessoa. O objeto deve ter as seguintes propriedades: nome e notas. Adicione um método chamado calcularMediaNotas ao objeto pessoa. Este método deve calcular a média das notas da pessoa.
+
+const pessoa = {
+    nome: 'Ronaldo Santos',
+    notas: [9.5, 7.0, 8.5],
+    calcularMediaNotas: function() {
+        const soma = this.notas.reduce((total, nota) => total + nota, 0);
+        return soma / this.notas.length;
+    },
+
+    classificarDesempenho: function() {
+        const media = this.calcularMediaNotas();
+
+        if (media >= 9) {
+            return "Desempenho excelente";
+        } else if (media >= 7.6 && media < 9) {
+            return "Bom desempenho";
+        } else if (media >= 6 && media < 7.6) {
+            return "Desempenho regular";
+        } else {
+            return "Desempenho insuficiente";
+        }
+    }
+};
+
+console.log("Média:", pessoa.calcularMediaNotas());
+console.log("Classificação:", pessoa.classificarDesempenho());

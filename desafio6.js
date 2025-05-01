da37d36e1e1e7c8f8efd06e5023807b74614fa9a
Gustavo Brandão <gustavo1731@hotmail.com>
2025-05-01 18:05:15 -0300

    atualização

--- desafio6.js
+++ desafio6.js
@@ -43,3 +43,84 @@ mostrarInfoPessoa(pessoa);
 mostrarInfoPessoa(pessoa);
 
 
+// Desafio 3: Crie uma lista de pessoas chamada pessoas que será um array contendo objetos. Cada objeto deve representar uma pessoa.
+
+let pessoas = [
+    {
+        nome: "Ana Silva",
+        idade: 28,
+        cidade: "São Paulo"
+    },
+    {
+        nome: "Carlos Souza",
+        idade: 35,
+        cidade: "Rio de Janeiro"
+    },
+    {
+        nome: "Mariana Costa",
+        idade: 22,
+        cidade: "Belo Horizonte"
+    }
+];
+
+function mostrarListaPessoas(lista) {
+    lista.forEach(pessoa => {           //passar por cada linha.
+        console.log(`${pessoa.nome}, ${pessoa.idade} anos, mora em ${pessoa.cidade}`);
+    });
+}
+
+pessoas.push({
+    nome: "José Silva",
+    idade: 25,
+    cidade: "Porto Alegre"
+})
+
+mostrarListaPessoas(pessoas);
+
+function filtrarPorCidade(lista, cidade) {
+    return lista.filter(pessoa => pessoa.cidade === cidade);
+}
+
+let moradoresDeSaoPaulo = filtrarPorCidade(pessoas, "São Paulo");
+
+mostrarListaPessoas(moradoresDeSaoPaulo);
+
+
+// Desafio 4: Crie um objeto chamado calculadora.
+
+let calculadora = {
+
+    soma: function(a, b) {
+        return a + b;
+    },
+
+    subtracao: function(a, b) {
+        return a - b;
+    },
+
+    multiplicacao: function(a, b) {
+        a * b;
+    },
+
+    divisao: function(a,b) {
+        if (b === 0) {
+            return 'Erro: divisão por zero!';
+        } else {
+            return a / b;
+        }
+    },
+
+    calcularMedia: function(numeros) {
+        const soma = numeros.reduce((total, numero) => total + numero, 0);
+        return soma / numeros.length;
+    }
+};
+
+console.log("Soma: " + calculadora.soma(5, 3));
+console.log("Subtração: " + calculadora.subtracao(8, 4));
+console.log("Multiplicação: " + calculadora.multiplicacao(6, 2));
+console.log("Divisão: " + calculadora.divisao(10, 2));
+console.log("Divisão por zero: " + calculadora.divisao(8, 0));
+
+const numerosParaMedia = [10, 8, 6, 9, 7];
+console.log("Média: " + calculadora.calcularMedia(numerosParaMedia));
